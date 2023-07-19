666baa83e13c56c1bde1f9caab879cdf6a21126f
Arie Pieter <ariepieter@yaranga.nl>
2023-07-19 20:51:08 +0000

    almost there

--- script.js
+++ script.js
@@ -1,68 +1,81 @@
 
-var coreFnData;
-var currentDepth = 0;
-var maxDepth = 0;
+let coreFnData;
+let currentDepth = 0;
+let maxDepth = 0;
 
-var margin = { top: 60, right: 20, bottom: 20, left: 20 };
-var width = window.innerWidth - margin.right - margin.left;
-var height = window.innerHeight - margin.top - margin.bottom;
+let margin = { top: 60, right: 20, bottom: 20, left: 20 };
+let width = window.innerWidth - margin.right - margin.left;
+let height = window.innerHeight - margin.top - margin.bottom;
 
+let recCoreFnElements =
+  ["decls"
+    , "expression"
+    , "body"
+    , "abstraction"
+    /*,"argument"*/
+    , "caseAlternatives"
+    , "caseExpressions"
+    , "binders"
+    , "literal"
+    , "value"
+    , "binds"
+  ]
 
 
 function submit() {
-  var elems = document.getElementsByTagName("svg");
+  let elems = document.getElementsByTagName("svg");
   while (elems.length > 0) {
     elems[0].remove();
   }
 
-  
+
   //elems.forEach(x=>x.remove());
   coreFnData = JSON.parse(document.getElementById("corefn-data").value);
-  
+  //console.log(coreFnData);
 
-  root = transformCoreFn(coreFnData['decls']);
-  for (const decl in root.children) {
-    update(decl);
-  }
-  //update(root);
-
+  let root = transformCoreFn("decls", coreFnData['decls'][2]);
+  //for (const decl in root.children) {
+  //  update(decl);
+  //}
+  update(root);
+  console.log(coreFnData);
 }
 
 
 
 
 function update(source) {
-  var i = 0;
+  let i = 0;
 
-var tree = d3.layout.tree()
+  let tree = d3.layout.tree()
     .size([height, width]);
 
-  var diagonal = d3.svg.diagonal()
+  let diagonal = d3.svg.diagonal()
     .projection(function (d) { return [d.x, d.y]; });
 
-  var svg = d3.select("body").append("svg")
+  let svg = d3.select("body").append("svg")
     .attr("width", width + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
 
   // Compute the new tree layout.
-  var nodes = tree.nodes(source).reverse();
-  var links = tree.links(nodes);
+  let nodes = tree.nodes(source).reverse();
+  let links = tree.links(nodes);
 
   nodes.forEach(function (d) { d.y = d.depth * 75; });
 
-  var scale = height / maxRec(source, "y");
+  let scale = height / maxRec(source, "y");
   svg.attr("transform", `scale(${scale}) translate(${margin.left}, ${margin.right})`);
 
   // Normalize for fixed-depth.
   nodes.forEach(function (d) { d.x = d.x / scale });
 
   // Declare the nodes…
-  var node = svg.selectAll("g.node")
+  let node = svg.selectAll("g.node")
     .data(nodes, function (d) { return d.id || (d.id = ++i); });
 
   // Enter the nodes.
-  var nodeEnter = node.enter().append("g")
+  let nodeEnter = node.enter().append("g")
     .attr("class", "node")
     .attr("transform", function (d) {
       return "translate(" + d.x + "," + d.y + ")";
@@ -81,26 +94,55 @@ var tree = d3.layout.tree()
     .attr("dy", ".35em")
     .attr("text-anchor", "middle")
     .text(function (d) {
-      if (d.type === 'Abs') {
-        return "\u03BB" + d.argument
-      }
-      else if (d.type === 'App') {
-        return '@'
-      }
-      else if (d.type !== undefined) {
-        return d.type;
-      }
-      else if (d.value !== undefined) {
-        return d.value;
-      }
-      else {
-        return d.identifier;
-      }
+      return d.label
+      // if (d.type === 'Abs') {
+      //   return "\u03BB" + d.argument
+      // }
+      // else if (d.type === 'App') {
+      //   return '@'
+      // }
+      // /*
+      // else if (d.type === 'Var') {
+      //   let ident = d.children.identifier
+      //   let module = ""
+      //   if (d.children.hasOwnProperty("moduleName")) {
+      //     d.children.moduleName.forEach(element => {
+      //       module = module + element + "."
+      //     });
+      //   }
+      //   return module + ident
+      // }
+      // */
+      // else if (d.type !== undefined) {
+      //   return d.type;
+      // }
+      // else if (d.value !== undefined) {
+      //   return d.value;
+      // }
+      // else if (d.hasOwnProperty("binderType")) {
+      //   return d.binderType
+      // }
+      // else {
+      //   if (d.hasOwnProperty("identifier")) {
+      //     let ident = d.identifier
+      //     let module = ""
+      //     // if (d.hasOwnProperty("moduleName")) {
+      //     //   d.moduleName.forEach(element => {
+      //     //     module = module + element + "."
+      //     //   });
+      //     // }
+      //     return module + ident
+      //   }
+      //   else {
+      //     return "[ ]"
+      //   }
+
+      // }
     })
     .style("fill-opacity", 1);
 
   // Declare the links…
-  var link = svg.selectAll("path.link")
+  let link = svg.selectAll("path.link")
     .data(links, function (d) { return d.target.id; });
 
   // Enter the links.
@@ -112,40 +154,128 @@ var tree = d3.layout.tree()
 
 
 
-function transformCoreFn(coreFn) {
+function transformCoreFn(key, coreFn) {
   currentDepth++;
   if (currentDepth > maxDepth) {
     maxDepth = currentDepth;
   }
 
-  var node = {};
-  for (const property in coreFn) {
-    let value = coreFn[property];
-    //console.log(property);
-    if (typeof (value) === 'object') {
-      if (property === "annotation" || property === "sourcePos" || property === "moduleName") {
-        //console.log("skip property")
-      }
-      else {
-        if (!node.hasOwnProperty('children')) {
-          node['children'] = [];
-        }
-        node['children'].push(transformCoreFn(value));
-      }
+
+  let node = { label: key, children: [] };
+  if (coreFn.hasOwnProperty("type")) {
+    if (coreFn.type === "App") {
+      node.label = "@"
+    }
+    else if (coreFn.type === "Abs") {
+      node.label = node.label = "\u03BB" + coreFn.argument
     }
     else {
-      let tObj = {};
-      tObj[property] = value;
-      Object.assign(node, tObj);
+      node.label = coreFn.type
     }
   }
+  if (coreFn.hasOwnProperty("identifier")) {
+    if (key === "value") {
+      let ns = ""
+      if (coreFn.hasOwnProperty("moduleName")) {
+        coreFn.moduleName.forEach(element => {
+          ns = ns + element + "."
+        });
+        //ns = ns + "\n"
+      }
+      node.label = ns + coreFn.identifier
+    }
+    node.label = coreFn.identifier
+  }
+
+
+
+  for (const property in coreFn) {
+    let value = coreFn[property];
+    if (typeof (value) === 'object') {
+      if (recCoreFnElements.includes(property) || Array.isArray()) {
+        node.children.push(transformCoreFn(property, value))
+      }
+    }
+
+
+  }
+
+
+
+
+  //console.log(property);
+  /*
+      if (!["annotation", "sourcePos", "moduleName"].includes(property)) {
+        if (value.hasOwnProperty("type")) {
+          let type = value.type
+          if (type === "App") {
+            node.label = "@"
+          }
+          else if (type === "Abs") {
+            let arg = value.argument
+            node.label = "\u03BB" + arg
+          }
+          else {
+            node.label = type
+          }
+        }
+        else {
+          if ()
+          node.label = property
+        }
+        if (typeof (value) === 'object') {
+          node.children = []
+          node.children.push(transformCoreFn(value))
+        }
+      }
+    }
+    /*
+        if (typeof (value) === 'object') {
+          if (property === "annotation" || property === "sourcePos" || property === "moduleName") {
+            //console.log("skip property")
+          }
+          else if (property === "value") {
+            let ns = ""
+    
+            if (value.hasOwnProperty("moduleName")) {
+              value.moduleName.forEach(element => {
+                ns = ns + element + "."
+              });
+              ns = ns + "\n"
+            }
+    
+            value.identifier = ns + value.identifier
+            
+            if (!node.hasOwnProperty('children')) {
+              node['children'] = [];
+            }
+            node['children'].push(transformCoreFn(value));
+            
+            
+            // let tObj = {};
+            // tObj[property] = ident
+            // Object.assign(node, tObj);
+          }
+          else {
+            if (!node.hasOwnProperty('children')) {
+              node['children'] = [];
+            }
+            node['children'].push(transformCoreFn(value));
+          }
+        }
+        else {
+          let tObj = {};
+          tObj[property] = value;
+          Object.assign(node, tObj);
+        }
+      }*/
   currentDepth--;
 
   return node;
 }
 
 function maxRec(object, key) {
-  var retVal = 0;
+  let retVal = 0;
   if (object !== undefined) {
     if (object.hasOwnProperty("children")) {
       retVal = Math.max(object[key], Math.max(...object.children.map(x => maxRec(x, key))));
@@ -157,3 +287,17 @@ function maxRec(object, key) {
 
   return retVal;
 }
+
+
+/*
+
+        if (d.hasOwnProperty("identifier")) {
+          let ident = d.identifier
+          let parts = ident.split(".")
+          return ident
+        }
+        else {
+          return "[ ]"
+        }
+
+        */
